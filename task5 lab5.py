e384227e9b8a7f4674a2ea5cb8eaa8868200c9dc
Aleksida <105102537+Aleksida@users.noreply.github.com>
2022-11-12 22:59:24 +0300

    Update task5 lab5.py

--- task5 lab5.py
+++ task5 lab5.py
@@ -5,7 +5,8 @@ n = 8
 
 def get_random_password() -> str:
     password_elements = str(string.ascii_lowercase+string.ascii_uppercase+string.digits) # TODO написать функцию генерации случайных паролей
-    password = random.sample(password_elements, n)
+    password_ = random.sample(password_elements, n)
+    password = ",".join(password_)
     return password
 
 
